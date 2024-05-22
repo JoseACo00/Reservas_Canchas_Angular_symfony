25f17546543d28596ef5a1e5c4a0b33204bd0613
Josele Coca <josealx0211@gmail.com>
2024-05-22 20:45:07 +0200

    Creacion de la tabla Cancha

---
+++ BackFootCap7/src/Entity/Cancha.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CanchaRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CanchaRepository::class)]
+class Cancha
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $localidad = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $direccion = null;
+
+    #[ORM\Column]
+    private ?float $precio = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fotos = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $disponibilidad = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $created_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $created_by = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $modified_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $modified_by = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $deleted_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $deleted_by = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getLocalidad(): ?string
+    {
+        return $this->localidad;
+    }
+
+    public function setLocalidad(string $localidad): static
+    {
+        $this->localidad = $localidad;
+
+        return $this;
+    }
+
+    public function getDireccion(): ?string
+    {
+        return $this->direccion;
+    }
+
+    public function setDireccion(string $direccion): static
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?float
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(float $precio): static
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    public function getFotos(): ?string
+    {
+        return $this->fotos;
+    }
+
+    public function setFotos(string $fotos): static
+    {
+        $this->fotos = $fotos;
+
+        return $this;
+    }
+
+    public function getDisponibilidad(): ?string
+    {
+        return $this->disponibilidad;
+    }
+
+    public function setDisponibilidad(string $disponibilidad): static
+    {
+        $this->disponibilidad = $disponibilidad;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?int
+    {
+        return $this->created_by;
+    }
+
+    public function setCreatedBy(?int $created_by): static
+    {
+        $this->created_by = $created_by;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeInterface
+    {
+        return $this->modified_at;
+    }
+
+    public function setModifiedAt(?\DateTimeInterface $modified_at): static
+    {
+        $this->modified_at = $modified_at;
+
+        return $this;
+    }
+
+    public function getModifiedBy(): ?int
+    {
+        return $this->modified_by;
+    }
+
+    public function setModifiedBy(?int $modified_by): static
+    {
+        $this->modified_by = $modified_by;
+
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeInterface
+    {
+        return $this->deleted_at;
+    }
+
+    public function setDeletedAt(\DateTimeInterface $deleted_at): static
+    {
+        $this->deleted_at = $deleted_at;
+
+        return $this;
+    }
+
+    public function getDeletedBy(): ?int
+    {
+        return $this->deleted_by;
+    }
+
+    public function setDeletedBy(?int $deleted_by): static
+    {
+        $this->deleted_by = $deleted_by;
+
+        return $this;
+    }
+}
