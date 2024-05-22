3e6f515390eb384dff38e99a9728714765f1e48f
Josele Coca <josealx0211@gmail.com>
2024-05-23 00:18:24 +0200

    El login está realizado pero se debe mejorar : Cambiar imagen y tener alog más claro

--- FrontFootcap7/src/app/app.module.ts
+++ FrontFootcap7/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule,  } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
